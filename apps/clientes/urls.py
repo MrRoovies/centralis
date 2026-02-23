5eaf8cc83334081867b9b98a54c8a677ffdca018
Rodrigo da Silva Matheus <rodrigo.matheus@11ponto11.local>
2026-02-23 11:27:30 -0300

    Url do cadastro cliente adicionada.

--- apps/clientes/urls.py
+++ apps/clientes/urls.py
@@ -6,4 +6,5 @@ app_name = 'clientes'
 urlpatterns = [
     path('search_cliente', views.search_cliente, name='search_cliente'),
     path('cliente_novo', views.cliente_novo, name='cliente_novo'),
+    path('cliente/<int:id>', views.cliente, name='cliente'),
 ]
