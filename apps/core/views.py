5e1286f79baaf64c3291549cc195ccd7de8ba912
Rodrigo da Silva Matheus <rodrigo.matheus@11ponto11.local>
2026-03-09 11:48:59 -0300

    fix: Alterado modelo de logon

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,29 +1,41 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from django.views.decorators.http import require_POST
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.decorators import login_required
 import json
+
 
 # Create your views here.
 def login_template(request):
     return render(request, "registration/login.html")
 
-@require_POST
+@require_http_methods(["POST"])
 def login_view(request):
-    if request.body:
-        data = json.loads(request.body)
-        username = data.get('text_user')
-        password = data.get('text_pass')
 
-        user = authenticate(request, username=username, password=password, empresa=request.empresa)
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+            empresa=request.empresa
+        )
 
         if user is None:
-            return JsonResponse({'status': 'error', 'message': 'Usuário ou senha incorretos'}, status=403)
+            return render(
+                request,
+                "registration/login.html",
+                {"error": "Usuário ou senha incorretos"}
+            )
 
         login(request, user)
-        return JsonResponse({'status': 'success', 'message': 'Bem vindo!'}, status=200)
+        return redirect("/home")
 
+
+@login_required
 def logout_view(request):
     if request.method == "POST":
         logout(request)
