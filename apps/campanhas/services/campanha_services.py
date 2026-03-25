c48ffe6e29b1caef011d9abe179d77bb87dbc38c
Rodrigo Matheus <52510942+MrRoovies@users.noreply.github.com>
2026-03-25 12:58:44 -0300

    feat: Add 'agenda' em related/feat: Add return para Cliente agendado com outro consultor

--- apps/campanhas/services/campanha_services.py
+++ apps/campanhas/services/campanha_services.py
@@ -1,4 +1,5 @@
 from apps.campanhas.models import Campanha, CampanhaCliente
+from django.utils import timezone
 
 class CampanhaService:
     @staticmethod
@@ -21,7 +22,7 @@ class CampanhaService:
         # Retorna lista de clientes vinculados ao agente para controle e atendimento #
         return (
             CampanhaCliente.objects
-            .select_related('cliente', 'situacao')
+            .select_related('cliente', 'situacao', 'agenda')
             .filter(
                 agente_responsavel=usuario,
                 campanha=campanha,
@@ -31,13 +32,13 @@ class CampanhaService:
         )
 
 
-
     @staticmethod
     def restantes_mailing(campanha):
         return (CampanhaCliente.objects.filter(
             campanha=campanha,
             situacao__tipo="INICIAL",
         ).count())
+
 
     @staticmethod
     def proximo_cliente_para_atendimento(campanha, usuario):
@@ -53,6 +54,18 @@ class CampanhaService:
         )
         if em_curso:
             return em_curso
+
+        agendado = (
+            CampanhaCliente.objects
+            .select_related('cliente', 'situacao')
+            .filter(
+                agente_responsavel=usuario,
+                campanha=campanha,
+                situacao__tipo="AGENDA",
+            ).first()
+        )
+        if agendado.agenda.data_hora_retorno < timezone.now():
+            return agendado
 
         # 2 Verifica se existe cliente em Fila
         proximo = (
