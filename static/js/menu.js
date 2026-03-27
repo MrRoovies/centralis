de839534dbcf2eb1d497e21fe8da69c5ab254cb1
Rodrigo Matheus <52510942+MrRoovies@users.noreply.github.com>
2026-03-27 09:36:58 -0300

    refactor: Mv csrf function para utils.js

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,20 +1,4 @@
 // main.js - Lógica principal do sistema
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
 document.querySelector(".search-icon").addEventListener('click', function(){
     const documento = document.getElementById("searchCliente").value;
 
