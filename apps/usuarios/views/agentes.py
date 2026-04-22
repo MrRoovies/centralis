37bf5815a3eaa2906457608d57e6e2f596f536fc
Rodrigo Matheus <52510942+MrRoovies@users.noreply.github.com>
2026-04-22 15:35:32 -0300

    fix: função 'campos_obrigatorios' - removido parametro inválido

--- apps/usuarios/views/agentes.py
+++ apps/usuarios/views/agentes.py
@@ -116,7 +116,7 @@ def editar_agente(request, agente_id):
         except json.JSONDecodeError:
             return JsonResponse({'success': False, 'messages': {'agente': {'__all__': ['JSON inválido']}}}, status=400)
 
-        campos_obrigatorios = AgenteService.campos_obrigatorios(data, empresa)
+        campos_obrigatorios = AgenteService.campos_obrigatorios(data)
         if not campos_obrigatorios['success']:
             return JsonResponse(campos_obrigatorios, status=400)
 
